5e687aa47e2bd056cd42516811040fa19c3b86bd
John A. De Goes <john@socialmedia.com>
2010-10-13 15:41:10 -0600

    add core project with netty dependency

--- project/build/BlueEyesProject.scala
+++ project/build/BlueEyesProject.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 class BlueEyesProject(info: ProjectInfo) extends ParentProject(info) {
-
   trait Repositories {
     val scalareleases   = "Scala Repo Releases"        at "http://scala-tools.org/repo-releases/"
     val scalasnapshots  = "Scala-tools.org Repository" at "http://scala-tools.org/repo-snapshots/"
+    val jbossreleases   = "JBoss Releases"             at "http://repository.jboss.org/nexus/content/groups/public/"
   }
-
-   class TestDepsProject(info: ProjectInfo) extends DefaultProject(info) {
-      val scalatest  = "org.scalatest"           % "scalatest"        % "1.2"    % "test"
-      val scalaspec  = "org.scala-tools.testing" % "specs"            % "1.6.1"  % "test"
-      val scalacheck = "org.scala-tools.testing" % "scalacheck_2.7.7" % "1.6"    % "test"
-      val mockito    = "org.mockito"             % "mockito-all"      % "1.8.4"  % "test"
+  
+  class TestDepsProject(info: ProjectInfo) extends DefaultProject(info) {
+    val scalatest  = "org.scalatest"           % "scalatest"        % "1.2"    % "test"
+    val scalaspec  = "org.scala-tools.testing" % "specs"            % "1.6.1"  % "test"
+    val scalacheck = "org.scala-tools.testing" % "scalacheck_2.7.7" % "1.6"    % "test"
+    val mockito    = "org.mockito"             % "mockito-all"      % "1.8.4"  % "test"
   }
 
   // hack to work around failure of inheriting dependencies via traits
@@ -19,7 +19,13 @@ class BlueEyesProject(info: ProjectInfo) extends ParentProject(info) {
 
   // Blueeyes projects
   lazy val health = project("health", "health",  new HealthProject(_), testDeps)
+  
+  lazy val core = project("core", "core", new CoreProject(_), testDeps)
 
   class HealthProject(info: ProjectInfo) extends DefaultProject(info) with Repositories {
+  }
+  
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) with Repositories {
+    val netty = "org.jboss.netty" % "netty" % "3.2.2" % "compile"
   }
 }
