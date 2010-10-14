7f5917c96ed5a41a6dae889d2bddf15772ec3227
Michael Lagutko <misha@socialmedia.com>
2010-10-14 21:53:47 +0300

    projects are merged

--- core/src/main/scala/blueeyes/core/service/HttpException.scala
+++
@@ -1,5 +0,0 @@
-package blueeyes.core.service
-
-case class HttpException(failure: HttpFailure, reason: String) extends Exception(reason) {
-  def this(failure: HttpFailure) = this(failure, failure.defaultMessage)
-}
--- core/src/main/scala/blueeyes/core/service/HttpStatus.scala
+++
@@ -1,5 +0,0 @@
-package blueeyes.core.service
-
-case class HttpStatus(code: HttpStatusCode, reason: String) {
-  def this(code: HttpStatusCode) = this(code, code.defaultMessage)
-}
