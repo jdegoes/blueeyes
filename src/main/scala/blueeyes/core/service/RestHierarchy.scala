d3d48ff55ad964a1d4fb8f633fafa3923a6c95f3
John A. De Goes <john@socialmedia.com>
2010-10-15 15:37:04 -0600

    fix type sig on rest path handler for resource hierarchy builder

--- src/main/scala/blueeyes/core/service/RestHierarchy.scala
+++ src/main/scala/blueeyes/core/service/RestHierarchy.scala
@@ -3,12 +3,14 @@ package blueeyes.core.service
 import HttpMethods._
 
 trait RestHierarchyBuilder[T] {
-  import scala.collection.mutable.{Stack}
+  import scala.collection.mutable.{Stack, ArrayBuffer}
+  
+  private type Handler[T] = (Map[Symbol, String], HttpRequest[T]) => Future[HttpResponse[T]]
   
   private val pathStack: Stack[RestPath] = new Stack[RestPath].push(RootPath);
-  private var _hierarchy: List[(RestPath, HttpMethod, RestPathHandler[T])] = Nil
+  private val _hierarchy: ArrayBuffer[(RestPath, HttpMethod, Handler[T])] = new ArrayBuffer
   
-  def hierarchy = _hierarchy
+  def hierarchy = _hierarchy.toList
   
   def path(path: RestPath)(f: => Unit): Unit = {
     pathStack.push(path)
@@ -16,24 +18,24 @@ trait RestHierarchyBuilder[T] {
     try { f } finally { pathStack.pop(path) }
   }
   
-  def get(handler: RestPathHandler[T]) = custom(GET, handler)
+  def get(handler: Handler[T]) = custom(GET, handler)
   
-  def put(handler: RestPathHandler[T]) = custom(PUT, handler)
+  def put(handler: Handler[T]) = custom(PUT, handler)
   
-  def post(handler: RestPathHandler[T]) = custom(POST, handler)
+  def post(handler: Handler[T]) = custom(POST, handler)
   
-  def delete(handler: RestPathHandler[T]) = custom(DELETE, handler)
+  def delete(handler: Handler[T]) = custom(DELETE, handler)
   
-  def options(handler: RestPathHandler[T]) = custom(OPTIONS, handler)
+  def options(handler: Handler[T]) = custom(OPTIONS, handler)
   
-  def head(handler: RestPathHandler[T]) = custom(HEAD, handler)
+  def head(handler: Handler[T]) = custom(HEAD, handler)
   
-  def connect(handler: RestPathHandler[T]) = custom(CONNECT, handler)
+  def connect(handler: Handler[T]) = custom(CONNECT, handler)
   
-  def trace(handler: RestPathHandler[T]) = custom(TRACE, handler)
+  def trace(handler: Handler[T]) = custom(TRACE, handler)
   
-  def custom(method: HttpMethod, handler: RestPathHandler[T]) = {
-    _hierarchy = (currentPath, method, handler) :: hierarchy
+  def custom(method: HttpMethod, handler: Handler[T]) = {
+    _hierarchy += (currentPath, method, handler)
   }
   
   private def currentPath = pathStack.foldRight(RestPath.Root) { (path, element) => path + element }
