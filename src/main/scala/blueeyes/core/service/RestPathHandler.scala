7a042464ffcfd98aa503f249c0751ec04796fd64
Michael Lagutko <misha@socialmedia.com>
2010-10-15 20:50:29 +0300

    Rest service implementation

---
+++ src/main/scala/blueeyes/core/service/RestPathHandler.scala
@@ -0,0 +1,24 @@
+package blueeyes.core.service
+
+import blueeyes.util.Future
+import util.matching.Regex
+
+case class RestPathHandler[T](pattern: PartialFunction[String, Map[Symbol, String]], handler: (Map[Symbol, String], HttpRequest[T]) => Future[HttpResponse[T]])
+                             extends PartialFunction[String, HttpRequest[T] => Future[HttpResponse[T]]] {
+ def isDefinedAt(url: String) = pattern.isDefinedAt(url)
+
+ def apply(url: String) = (request: HttpRequest[T]) => {
+   val symbols = pattern(url)
+
+   handler(symbols, request)
+ }
+}
+
+sealed trait RestPath {
+  def elements: List[PathElement]
+}
+
+sealed trait PathElement
+case class StringElement(element: String)
+case class SymbolElement(element: Symbol)
+case class RegexElement(element: Regex)
---
+++ src/test/scala/blueeyes/core/service/RestPathHandlerSpec.scala
@@ -0,0 +1,9 @@
+package blueeyes.core.service
+
+import org.specs.ScalaCheck
+import org.spex.Specification
+class RestPathHandlerSpec extends Specification with ScalaCheck{
+  "RestPathHandler handle request when pattern is match" in {
+        
+  }
+}
