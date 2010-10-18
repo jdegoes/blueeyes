3ac92f1b82852cca7529066fd9a088d450296a6d
John A. De Goes <john@socialmedia.com>
2010-10-18 11:47:18 -0600

    update bijection definition

--- src/main/scala/blueeyes/util/Bijection.scala
+++ src/main/scala/blueeyes/util/Bijection.scala
@@ -1,11 +1,14 @@
 package blueeyes.core.service
 
-trait Bijection[T, S] {
+trait Bijection[T, S] { self =>
   def apply(t: T): S
   
-  def inverse: Bijection[S, T]
+  def unapply(s: S): T
   
-  def unapply(s: S): T = inverse.apply(s)
+  def inverse: Bijection[S, T] = new Bijection[S, T] {
+    def apply(s: S): T   = self.unapply(s)
+    def unapply(t: T): S = self.apply(t)
+  }
 }
 
 trait DataTranscoder[T, S] {
