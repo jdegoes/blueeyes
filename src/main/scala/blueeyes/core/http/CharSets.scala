db342f2f7c4a4db39e37cfb0365cf1fbc50fc2bf
Kashomon <jrhoak@gmail.com>
2010-10-21 17:04:14 -0600

    More fixes to CharSets

--- src/main/scala/blueeyes/core/http/CharSets.scala
+++ src/main/scala/blueeyes/core/http/CharSets.scala
@@ -26,7 +26,7 @@ object CharSets {
             case "ISO-8859-1" => `ISO-8859-1`
             case "ISO-8859-2" => `ISO-8859-2`
             case "ISO-8859-3" => `ISO-8859-3`
-            case "ISO-8859-4" => `iso-8859-4`
+            case "ISO-8859-4" => `ISO-8859-4`
             case "ISO-8859-5" =>  `ISO-8859-5`
             case "ISO-8859-6" =>  `ISO-8859-6`
             case "ISO-8859-7" =>  `ISO-8859-7`
