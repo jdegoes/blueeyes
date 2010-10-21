904940b787b76c30853f63b221d5957c76236290
Michael Lagutko <misha@socialmedia.com>
2010-10-21 21:50:42 +0300

    BlueEyesServiceSpecification implementation: Sample is added

--- src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
+++ src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
@@ -5,37 +5,34 @@ import blueeyes.core.service.RestPathPatternImplicits._
 import blueeyes.core.service._
 import blueeyes.util.Future
 
-class BlueEyesServiceSpecificationSpec extends Specification {
-  private val specification = new TestBlueEyesServiceSpecification()
+class BlueEyesServiceSpecificationSpec extends Specification with BlueEyesServiceSpecification[String]{
+  val serviceResponse = HttpResponse[String](HttpStatus(HttpStatusCodes.OK), Map("Content-Type" -> "text/html"), Some("context"), HttpVersions.`HTTP/1.1`)
 
   "calls test function" in {
     var executed = false
-    specification.path("/bar/'id/bar.html"){
-      specification.get{
+    path("/bar/'id/bar.html"){
+      get{
         executed = true
       }
     }
     executed mustEqual (true)
   }
   "gets responce" in {
-    specification.path("/bar/'id/bar.html"){
-      specification.get{
-        specification.response mustEqual (specification.serviceResponse)         
+    path("/bar/'id/bar.html"){
+      get{
+        response mustEqual (serviceResponse)
       }
     }
   }
   "gets responce when future is set asynchronously" in {
-    specification.path("/asynch/future"){
-      specification.get{
-        specification.response mustEqual (specification.serviceResponse)
+    path("/asynch/future"){
+      get{
+        response mustEqual (serviceResponse)
       }
     }
   }
-}
 
-class TestBlueEyesServiceSpecification extends BlueEyesServiceSpecification[String]{
   val service = new Service()
-  val serviceResponse = HttpResponse[String](HttpStatus(HttpStatusCodes.OK), Map("Content-Type" -> "text/html"), Some("context"), HttpVersions.`HTTP/1.1`)
 
   class Service extends RestHierarchyBuilder[String] {
     path("/bar/'foo/bar.html") {get(new Handler())}
@@ -47,7 +44,7 @@ class TestBlueEyesServiceSpecification extends BlueEyesServiceSpecification[Stri
   class AsynchHandler extends Function1[HttpRequest[String], Future[HttpResponse[String]]]{
     def apply(request: HttpRequest[String]) = {
       import scala.actors.Actor._
-      
+
       val future = new Future[HttpResponse[String]]()
       val actor2 = actor {
         Thread.sleep(1000)
@@ -55,7 +52,6 @@ class TestBlueEyesServiceSpecification extends BlueEyesServiceSpecification[Stri
       }
       future
     }
-  }
+  }  
 }
-
 
