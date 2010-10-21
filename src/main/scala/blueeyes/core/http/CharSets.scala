a2c8d0412a77d99b8b398cc38b406d743d8f9cec
Kashomon <jrhoak@gmail.com>
2010-10-21 17:03:30 -0600

    fixes for charsets

--- src/main/scala/blueeyes/core/http/CharSets.scala
+++ src/main/scala/blueeyes/core/http/CharSets.scala
@@ -27,12 +27,12 @@ object CharSets {
             case "ISO-8859-2" => `ISO-8859-2`
             case "ISO-8859-3" => `ISO-8859-3`
             case "ISO-8859-4" => `iso-8859-4`
-            case "ISO-8859-5" =  `ISO-8859-5`
-            case "ISO-8859-6" =  `ISO-8859-6`
-            case "ISO-8859-7" =  `ISO-8859-7`
-            case "ISO-8859-8" =  `ISO-8859-8`
-            case "ISO-8859-9" =  `ISO-8859-9`
-            case "ISO-8859-10" =  `ISO-8859-106
+            case "ISO-8859-5" =>  `ISO-8859-5`
+            case "ISO-8859-6" =>  `ISO-8859-6`
+            case "ISO-8859-7" =>  `ISO-8859-7`
+            case "ISO-8859-8" =>  `ISO-8859-8`
+            case "ISO-8859-9" =>  `ISO-8859-9`
+            case "ISO-8859-10" =>  `ISO-8859-10`
             case _ => new CustomCharSet(charSet)
           }
         )
