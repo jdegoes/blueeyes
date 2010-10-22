33426b504bdab22e110bf8d64f3cdacff22ef060
Michael Lagutko <misha@socialmedia.com>
2010-10-22 20:53:35 +0300

    added some MongoPrimitives

---
+++ src/test/scala/blueeyes/persistence/mongo/MongoQueryBuilderSpec.scala
@@ -0,0 +1,12 @@
+package blueeyes.persistence
+
+import org.specs.Specification
+import MongoQueryImplicits._
+
+
+class MongoQueryBuilderSpec extends Specification{
+
+  "builds MongoFieldQuery $eq operation" in {
+    "foo".===  
+  }
+}
