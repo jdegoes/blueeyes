bcad7942fa47c6d36578c99aae4cd227d3ce55a3
Jeff Simpson <jsimpson@ziaconsulting.com>
2010-10-23 11:06:28 -0600

    Added more tests for the QueryParser

--- src/main/scala/blueeyes/util/QueryParser.scala
+++ src/main/scala/blueeyes/util/QueryParser.scala
@@ -1,23 +1,28 @@
 package blueeyes.util
 
+/**
+ * This object only deals with the query portion of the URI.  A URI can be decomposed into
+ * constituant components using the java.lang.URI class.
+ * Not:  The individual parameters/values are assumed to be encoded, if neccesary
+ */
 object QueryParser {
+  val encoding = "UTF-8"
+
   def parseQuery(query: String): Map[Symbol, String] = Map((query.split("&").toList.flatMap { nameEqualsValue =>
     nameEqualsValue.split("=").toList match {
       case "" :: Nil => Nil
-      case name :: value :: Nil => (name, value) :: Nil
       case name :: Nil => (name, "") :: Nil
+      case name :: value :: Nil => (name, value) :: Nil
       case name :: everythingElse => (name, everythingElse.mkString("=")) :: Nil
       case Nil => Nil
     }
   }).map { nameValue =>
     import java.net.URLDecoder._
-
-    (Symbol(decode(nameValue._1, "UTF-8")), decode(nameValue._2, "UTF-8"))
+    (Symbol(decode(nameValue._1, encoding)), decode(nameValue._2, encoding))
   }: _*)
 
   def unparseQuery(query: Map[Symbol, String]): String = query.map { nameValue =>
     import java.net.URLEncoder._
-
-    encode(nameValue._1.name, "UTF-8") + "=" + encode(nameValue._2, "UTF-8")
-  }.mkString("&")
+    (encode(nameValue._1.name, encoding) :: encode(nameValue._2, encoding) :: Nil).filter(n => 
+      !n.isEmpty).mkString("=")}.mkString("&")
 }
--- src/test/scala/blueeyes/util/QueryParserSpec.scala
+++ src/test/scala/blueeyes/util/QueryParserSpec.scala
@@ -1,16 +1,85 @@
 package blueeyes.util
 
 import QueryParser._
+import java.net.URI
 import org.specs.Specification
 import org.specs.util._
-import java.net.URI
+import java.net.URLEncoder._
 
 class QueryParserSpec extends Specification {
   val baseURI = "http://www.socialmedia.com/test?"
-    
+  val encoding = "UTF-8"
+
   "Support 'normal' query params" in {
     val queryParams = "a=1&b=2"
     val query = URI.create(baseURI + queryParams).getRawQuery()
-    unparseQuery(parseQuery(query)) must beEqual(queryParams)
+    var params = parseQuery(query)
+    params must (haveKey('a) and haveKey('b))
+    params must (havePair(('a, "1")) and havePair(('b, "2")))
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support value-less query params" in {
+    val queryParams = "usermode"
+    val query = URI.create(baseURI + queryParams).getRawQuery()
+    var params = parseQuery(query)
+    params must haveKey('usermode)
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support empty query string" in {
+    val queryParams = ""
+    val query = URI.create(baseURI + queryParams).getRawQuery()
+    var params = parseQuery(query)
+    params must beEmpty
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support query string with fragment appended" in {
+    val queryParams = "flag=true"
+    val query = URI.create(baseURI + queryParams + "#fragment").getRawQuery()
+    var params = parseQuery(query)
+    params must havePairs(('flag, "true"))
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support query string with <space>" in {
+    val queryParams = "flag=true&path=" + encode("/hello world", encoding)
+    val query = URI.create(baseURI + queryParams).getRawQuery
+    var params = parseQuery(query)
+    params must havePair(('flag, "true"))
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support query string with extra '?' in param name" in {
+    val queryParams = "flag=true&" + encode("path?", encoding) + "=foo"
+    val query = URI.create(baseURI + queryParams).getRawQuery
+    var params = parseQuery(query)
+    params must (havePair(('flag, "true")) and havePair((Symbol("path?"), "foo")))
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support query string with random '?'" in {
+    val queryParams = "flag=true&" + encode("path??path2", encoding)
+    val query = URI.create(baseURI + queryParams).getRawQuery
+    var params = parseQuery(query)
+    params must (havePair(('flag, "true")) and havePair((Symbol("path??path2"), "")))
+    unparseQuery(params) must beEqual(queryParams)
+  }
+
+  "Support empty parameter block '&&'" in {
+    val queryParams = "flag=true&&foo=bar"
+    val query = URI.create(baseURI + queryParams).getRawQuery
+    var params = parseQuery(query)
+    params must (havePair(('flag, "true")) and havePair(('foo, "bar")))
+    unparseQuery(params) must beEqual(queryParams.replace("&&", "&"))
+  }
+
+  "Support empty URI as param value" in {
+    val queryParams = "site=" + encode("http://www.google.com?search=blah", encoding)
+    val query = URI.create(baseURI + queryParams).getRawQuery
+    var params = parseQuery(query)
+    params must havePair(('site, "http://www.google.com?search=blah"))
+    unparseQuery(params) must beEqual(queryParams)
   }
 }
