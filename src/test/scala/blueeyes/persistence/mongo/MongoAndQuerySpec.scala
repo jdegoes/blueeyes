4a9b2383790fb4f8d434c945e347a8848ec93640
Michael Lagutko <misha@socialmedia.com>
2010-10-24 13:07:27 +0300

    MongoQuery implementation

--- src/test/scala/blueeyes/persistence/mongo/MongoAndQuerySpec.scala
+++ src/test/scala/blueeyes/persistence/mongo/MongoAndQuerySpec.scala
@@ -6,20 +6,18 @@ import blueeyes.json.JsonAST._
 import blueeyes.json._
 
 class MongoAndQuerySpec extends Specification{
-  "create valid json for or query" in {
-    import MongoQueryImplicits._
-    
-    val query1 = MongoQueryBuilder(JPath("foo")).>(1)
-    val query2 = MongoQueryBuilder(JPath("bar")).<(5)
+  private val query1    = MongoQueryBuilder(JPath("foo")).>(MongoQueryImplicits.MongoPrimitiveInt(1))
+  private val query2    = MongoQueryBuilder(JPath("bar")).<(MongoQueryImplicits.MongoPrimitiveInt(5))
+  private val andQuery  = query1 && query2
 
-    (query1 && query2).query mustEqual (JObject(query1.query.fields ++ query2.query.fields))
+  "create valid json for or query" in {
+    (andQuery).query mustEqual (JObject(query1.query.fields ++ query2.query.fields))
   }
   "unary_! use 'or' use with negative operators of subquerys " in{
-    import MongoQueryImplicits._
-    val query1 = MongoQueryBuilder(JPath("foo")).>(1)
-    val query2 = MongoQueryBuilder(JPath("bar")).<(5)
-
-    (query1 && query2).unary_! mustEqual (query1.unary_! || query2.unary_!)
+    (andQuery).unary_! mustEqual (query1.unary_! || query2.unary_!)
   }
 
+  "2 unary_! result to the same query" in{
+    (andQuery).unary_!.unary_! mustEqual (andQuery)
+  }
 }
--- src/test/scala/blueeyes/persistence/mongo/MongoOrQuerySpec.scala
+++ src/test/scala/blueeyes/persistence/mongo/MongoOrQuerySpec.scala
@@ -6,18 +6,17 @@ import blueeyes.json.JsonAST._
 import blueeyes.json._
 
 class MongoOrQuerySpec extends Specification{
+  private val query1  = MongoQueryBuilder(JPath("foo")).>(MongoQueryImplicits.MongoPrimitiveInt(1))
+  private val query2  = MongoQueryBuilder(JPath("bar")).<(MongoQueryImplicits.MongoPrimitiveInt(5))
+  private val orQuery = query1 || query2
+  
   "create valid json for or query" in {
-    import MongoQueryImplicits._
-    val query1 = MongoQueryBuilder(JPath("foo")).>(1)
-    val query2 = MongoQueryBuilder(JPath("bar")).<(5)
-
-    (query1 || query2).query mustEqual (JObject(JField("$or", JArray(query1.query :: query2.query :: Nil)) :: Nil))
+    (orQuery).query mustEqual (JObject(JField("$or", JArray(query1.query :: query2.query :: Nil)) :: Nil))
   }
   "unary_! use 'and' use with negative operators of subquerys " in{
-    import MongoQueryImplicits._
-    val query1 = MongoQueryBuilder(JPath("foo")).>(1)
-    val query2 = MongoQueryBuilder(JPath("bar")).<(5)
-
-    (query1 || query2).unary_! mustEqual (query1.unary_! && query2.unary_!)
+    (orQuery).unary_! mustEqual (query1.unary_! && query2.unary_!)
   }  
+  "2 unary_! results to the same query" in{
+    (orQuery).unary_!.unary_! mustEqual (orQuery)
+  }
 }
