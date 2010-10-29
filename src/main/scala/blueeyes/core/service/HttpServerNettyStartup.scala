444352361dab5315afaa293f2f0c998eac1936ce
Michael Lagutko <misha@socialmedia.com>
2010-10-29 21:24:59 +0300

    HttpServerNettyStartup is added

--- src/main/scala/blueeyes/core/service/HttpServerNettyStartup.scala
+++ src/main/scala/blueeyes/core/service/HttpServerNettyStartup.scala
@@ -1,6 +1,26 @@
 package blueeyes.core.service
 
+import blueeyes.persistence.mongo.RealMongo.RealMongoModule
+import blueeyes.config.{FilesystemConfiggyModule, ConfiggyModule}
+import com.google.inject.Guice
+import net.lag.configgy.Config
 
-class HttpServerNettyStartup{
-  
+object HttpServerNettyStartup{
+  private lazy val injector = Guice.createInjector(new FilesystemConfiggyModule(ConfiggyModule.FileLoc), new RealMongoModule)
+  private lazy val configgy = injector.getInstance(classOf[Config])
+
+  def main(args: Array[String]){
+    val servicesName    = configgy.getList("server.services")
+    val servicesClasses = servicesName.map(Class.forName(_).asInstanceOf[Class[HttpService[_ <: HttpService[_]]]])
+
+    val serverNetty     = new HttpServerNetty(List(servicesClasses: _*))
+
+    serverNetty.start(configgy.getInt("server.port").getOrElse(error("Server port is not configured. Congigure 'server.port'.")))
+
+    Runtime.getRuntime()addShutdownHook(new Thread(new Runnable{
+      def run = {
+        serverNetty.stop        
+      }
+    }))
+  }
 }
