9d9bb3fed4d50ba9acd367688a1ae2310872b40b
John A. De Goes <john@socialmedia.com>
2010-11-10 14:20:37 -0700

    add javadoc

--- src/main/scala/blueeyes/BlueEyesServiceBuilder.scala
+++ src/main/scala/blueeyes/BlueEyesServiceBuilder.scala
@@ -5,6 +5,16 @@ import blueeyes.core.service._
 import blueeyes.util.{Future, FutureImplicits}
 
 /** Convenience trait for building services with many common mixins. 
+ * <pre>
+ * trait EmailServices extends BlueEyesServiceBuilder {
+ *   val emailService = service("email", "1.32") {
+ *     request {
+ *       ...
+ *     }
+ *   }
+ * }
+ * object EmailServer extends BlueEyesServer with EmailService
+ * </pre>
  */
 trait BlueEyesServiceBuilder[T] extends HttpServiceBuilder[T] with 
   FutureImplicits with 
