662828847c3de05f763943559f73da8f1d7ac28d
John A. De Goes <john@socialmedia.com>
2010-11-10 18:15:43 -0700

    add spec for request handler combinators

---
+++ src/test/scala/blueeyes/core/service/HttpRequestHandlerCombinatorsSpec.scala
@@ -0,0 +1,11 @@
+package blueeyes.core.service
+
+import org.specs.Specification
+
+import blueeyes.core.http._
+
+class HttpRequestHandlerCombinatorsSpec extends Specification{
+  import HttpRequestHandlerCombinators._
+  
+  
+}
