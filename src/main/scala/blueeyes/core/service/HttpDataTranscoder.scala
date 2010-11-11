26de035b82d03cf7d3dfab5c59810633d3e5703a
Michael Lagutko <misha@socialmedia.com>
2010-11-11 17:39:24 +0200

    HttpResponseType is removed

--- src/main/scala/blueeyes/core/service/HttpDataTranscoder.scala
+++ src/main/scala/blueeyes/core/service/HttpDataTranscoder.scala
@@ -5,25 +5,15 @@ import blueeyes.core.http.MimeType
 import blueeyes.core.http.MimeTypes._
 import blueeyes.json.JsonAST.JValue
 
-sealed trait HttpResponseType[T]
-
-case object HttpResponseStringType extends HttpResponseType[String]
-case object HttpResponseBytesType  extends HttpResponseType[Array[Byte]]
-
 trait HttpDataTranscoder[T, S]{ self =>
-  def responseType: HttpResponseType[S]
-  
   def transcode: Bijection[T, S]
   
   def mimeType: MimeType
 }
 
-class HttpStringDataTranscoder[T](val transcode: Bijection[T, String], val mimeType: MimeType) extends HttpDataTranscoder[T, String]{
-  val responseType: HttpResponseType[String] = HttpResponseStringType
-}
-class HttpBytesDataTranscoder[T](val transcode: Bijection[T, Array[Byte]], val mimeType: MimeType) extends HttpDataTranscoder[T, Array[Byte]]{
-  val responseType: HttpResponseType[Array[Byte]] = HttpResponseBytesType
-}
+class HttpStringDataTranscoder[T](val transcode: Bijection[T, String], val mimeType: MimeType) extends HttpDataTranscoder[T, String]
+
+class HttpBytesDataTranscoder[T](val transcode: Bijection[T, Array[Byte]], val mimeType: MimeType) extends HttpDataTranscoder[T, Array[Byte]]
 
 object Transcoders{
   implicit val HttpJsonToText = new HttpStringDataTranscoder[JValue](Bijections.JValueToString, application/json)
