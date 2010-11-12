45f74e3ff5bf6a381c7736ee258843995e8bd875
Michael Lagutko <misha@socialmedia.com>
2010-11-12 18:32:02 +0200

    bug fixing

--- src/main/scala/blueeyes/core/service/NettyRequestHandler.scala
+++ src/main/scala/blueeyes/core/service/NettyRequestHandler.scala
@@ -1,15 +1,17 @@
 package blueeyes.core.service
 
 import scala.collection.JavaConversions._
-import org.jboss.netty.channel.{ChannelFutureListener, ChannelHandlerContext, MessageEvent, SimpleChannelUpstreamHandler}
 import org.jboss.netty.handler.codec.http.HttpHeaders.Names
 import org.jboss.netty.handler.codec.http.HttpHeaders._
+import blueeyes.util.FutureImplicits
 import Converters._
 import org.jboss.netty.handler.codec.http.{CookieEncoder, CookieDecoder, HttpRequest => NettyHttpRequest, HttpResponse => NettyHttpResponse, DefaultHttpResponse, HttpVersion => NettyHttpVersion}
 
 import blueeyes.core.http.{HttpMethod, HttpStatusCodes, HttpRequest, HttpResponse, HttpStatus}
 import org.jboss.netty.handler.codec.http._
 import blueeyes.util.Future
+import net.lag.logging.Logger
+import org.jboss.netty.channel._
 
 class NettyRequestHandler(hierarchies: List[RestHierarchy[_]]) extends SimpleChannelUpstreamHandler{
   override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) {
@@ -44,6 +46,8 @@ class NettyRequestHandler(hierarchies: List[RestHierarchy[_]]) extends SimpleCha
       response.addHeader(Names.SET_COOKIE, cookieEncoder.encode())
     }
   }
+
+  override def exceptionCaught(ctx: ChannelHandlerContext, e: ExceptionEvent) = {}
 }
 
 class NotFoundBuilder extends Function1[(String, HttpMethod), Future[NettyHttpResponse]]{
@@ -51,15 +55,31 @@ class NotFoundBuilder extends Function1[(String, HttpMethod), Future[NettyHttpRe
 }
 
 class RequestBuilder[S](event: MessageEvent, hierarchy: RestHierarchy[S]) extends PartialFunction[(String, HttpMethod), Future[NettyHttpResponse]]{
+  private val log = Logger.get
+  
   def isDefinedAt(uriAndMethod: (String, HttpMethod)) = findPattern(uriAndMethod._1, uriAndMethod._2).map(v => true).getOrElse(false)
   def apply(uriAndMethod: (String, HttpMethod)) = createResponse(uriAndMethod._1, event, findPattern(uriAndMethod._1, uriAndMethod._2).get)
 
   private def createResponse[T, S](requestUri: String, event: MessageEvent, handler: (RestPathPattern, HttpMethod, HttpRequest[T] => Future[HttpResponse[T]], HttpDataTranscoder[T, S])) = {
-    val request     = event.getMessage().asInstanceOf[NettyHttpRequest]
-    val parameters  = handler._1(requestUri)
+    try {
+      val request = event.getMessage().asInstanceOf[NettyHttpRequest]
+      val parameters = handler._1(requestUri)
 
-    handler._3(fromNettyRequest(request, parameters, event.getRemoteAddress, handler._4)).map(response => toNettyResponse(response, handler._4))
+      handler._3(fromNettyRequest(request, parameters, event.getRemoteAddress, handler._4)).map(response => {
+        try {
+          toNettyResponse(response, handler._4)
+        }
+        catch {
+          case e: Throwable => log.error(e, "Error while NettyResponse creation. Request:" + event);  errorResponse(e.getMessage);
+        }
+      })
+    }
+    catch {
+      case e: Throwable => log.error(e, "Error while request handling. Request:" + event); new Future[NettyHttpResponse]().deliver(errorResponse(e.getMessage));
+    }
   }
+
+  private def errorResponse(reason: String): NettyHttpResponse = new DefaultHttpResponse(NettyHttpVersion.HTTP_1_1, toNettyStatus(HttpStatus(HttpStatusCodes.InternalServerError, reason.replace("\n", ""))))
 
   private def findPattern(uri: String, method: HttpMethod) = hierarchy.hierarchy.find(handler => handler._1.isDefinedAt(uri) && method == handler._2)
 }
