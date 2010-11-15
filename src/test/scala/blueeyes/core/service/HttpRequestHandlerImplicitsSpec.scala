82d4ee4cf8f7054bde2fd1ddba94cd76a8fe2264
John A. De Goes <john@socialmedia.com>
2010-11-15 13:01:47 -0700

    remove unused transcoding, add spec for request handler implicits

---
+++ src/test/scala/blueeyes/core/service/HttpRequestHandlerImplicitsSpec.scala
@@ -0,0 +1,13 @@
+package blueeyes.core.service
+
+import org.specs.Specification
+
+import blueeyes.json.JsonAST._
+import blueeyes.util.Future
+import blueeyes.core.http._
+import blueeyes.core.data.DataTranscoders
+
+class HttpRequestHandlerImplicitsSpec extends Specification with HttpRequestHandlerImplicits {
+
+}
+
