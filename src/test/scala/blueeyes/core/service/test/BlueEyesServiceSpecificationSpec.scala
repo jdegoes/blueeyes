5f9a26c21626aef1e6cd854287aa3ae202b7f487
Michael Lagutko <misha@socialmedia.com>
2010-11-18 20:49:54 +0200

    BlueEyesServiceSpecification extsnds HTTPServer

--- src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
+++ src/test/scala/blueeyes/core/service/test/BlueEyesServiceSpecificationSpec.scala
@@ -8,62 +8,60 @@ import blueeyes.core.http.MimeTypes._
 import blueeyes.BlueEyesServiceBuilderString
 import blueeyes.core.http.MimeTypes._
 import blueeyes.core.http._
+import TestService._
 
-class BlueEyesServiceSpecificationSpec extends Specification with BlueEyesServiceSpecification[String] {
-  val serviceResponse = HttpResponse[String](HttpStatus(HttpStatusCodes.OK), Map("Content-Type" -> "text/html"), Some("context"), HttpVersions.`HTTP/1.1`)
-
-  val service = new SampeService().sampleService
-  def config = ""  
-
-  shareVariables()
+class BlueEyesServiceSpecificationSpec extends BlueEyesServiceSpecification[String] with TestService{
 
   "SampeService" should{
-    doFirst{start(60000)}
-
-  "calls test function" in {
-    var executed = false
-    path("/bar/id/bar.html"){
-      get{
-        executed = true
+    "calls test function" in {
+      var executed = false
+      path("/bar/id/bar.html"){
+        get{
+          executed = true
+        }
+      }
+      executed mustEqual (true)
+    }
+    "gets responce" in {
+      path("/bar/id/bar.html"){
+        get{
+          response mustEqual (serviceResponse)
+          ()
+        }
       }
     }
-    executed mustEqual (true)
-  }
-  "gets responce" in {
-    path("/bar/id/bar.html"){
-      get{
-        response mustEqual (serviceResponse)
-      }
-    }
-  }
-  "gets responce when future is set asynchronously" in {
-    path("/asynch/future"){
-      get{
-        response mustEqual (serviceResponse)
-      }
-    }
-  }
-    doLast{stop(60000)}
-  }
-
-  class SampeService extends BlueEyesServiceBuilderString {
-    val sampleService = service("sample", "1.32") { context =>
-      request { 
-        contentType(text/html) {
-          path("/bar/'foo/bar.html") {
-            get [String] { request: HttpRequest[String] =>
-              serviceResponse
-            }
-          } ~
-          path("/asynch/future") {
-            get [String]{ request: HttpRequest[String] =>
-              Future.async {
-                serviceResponse
-              }
-            }
-          }  
+    "gets responce when future is set asynchronously" in {
+      path("/asynch/future"){
+        get{
+          response mustEqual (serviceResponse)
+          ()
         }
       }
     }
   }
+}
+
+trait TestService extends BlueEyesServiceBuilderString {
+  val sampleService = service("sample", "1.32") { context =>
+    request {
+      contentType(text/html) {
+        path("/bar/'foo/bar.html") {
+          get [String] { request: HttpRequest[String] =>
+            serviceResponse
+          }
+        } ~
+        path("/asynch/future") {
+          get [String]{ request: HttpRequest[String] =>
+            Future.async {
+              serviceResponse
+            }
+          }
+        }
+      }
+    }
+  }
+} 
+
+object TestService{
+  val serviceResponse = HttpResponse[String](HttpStatus(HttpStatusCodes.OK), Map("Content-Type" -> "text/html"), Some("context"), HttpVersions.`HTTP/1.1`)
 }
