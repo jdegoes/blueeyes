9edf9ef770afef67ad956150e8c448ad19124768
Kashomon <jrhoak@gmail.com>
2010-11-19 16:22:46 -0700

    Added a test that fails to compile for 3 entries.

--- src/test/scala/blueeyes/persistence/mongo/MongoUpdateFieldsValuesSpec.scala
+++ src/test/scala/blueeyes/persistence/mongo/MongoUpdateFieldsValuesSpec.scala
@@ -10,6 +10,13 @@ import blueeyes.json.JsonAST._
 class MongoUpdateFieldsValuesSpec extends Specification{
   "build valid json with several MongoUpdateFieldValue" in{
     import MongoFilterImplicits._
-    (("x" inc (1)) & (("y" set (1)))).toJValue mustEqual (JObject(JField("$inc", JObject(JField("x", JInt(1)) :: Nil)) :: JField("$set", JObject(JField("y", JInt(1)) :: Nil)) :: Nil))
+    (("x" inc (1)) & ("y" set (1))).toJValue mustEqual (JObject(JField("$inc", JObject(JField("x", JInt(1)) :: Nil)) :: JField("$set", JObject(JField("y", JInt(1)) :: Nil)) :: Nil))
+  }
+
+  "build valid json with 3 MongoUpdateFieldValues" in{
+    import MongoFilterImplicits._
+    /*
+    (("x" inc (1)) & ("y" set (1)) & ("z" inc (1))).toJValue mustEqual (JObject(JField("$inc", JObject(JField("x", JInt(1)) :: Nil)) :: JField("$set", JObject(JField("y", JInt(1)) :: Nil)) :: JField("$inc", JObject(JField("z", JInt(1)) :: Nil)) :: Nil))
+    */
   }
 }
