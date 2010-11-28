50e7bf2e1b42ccff75e65b32bf8143e39629a44a
John A. De Goes <john@socialmedia.com>
2010-11-27 21:23:45 -0700

    make http client a request handler

--- src/main/scala/blueeyes/core/service/HttpClient.scala
+++ src/main/scala/blueeyes/core/service/HttpClient.scala
@@ -3,7 +3,9 @@ package blueeyes.core.service
 import blueeyes.util.Future
 import blueeyes.core.http._
 
-trait HttpClient[T]{
+trait HttpClient[T] extends HttpRequestHandler[T] {
+  def isDefinedAt(request: HttpRequest[T]): Boolean = true
+  
   def apply(request: HttpRequest[T]): Future[HttpResponse[T]]
 
   def exec[R](f: HttpClient[T] => Future[R]) = f(this)
