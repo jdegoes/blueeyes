f04c13918c1182ca291af5050fa5e1454cb8cd5e
John A. De Goes <john@socialmedia.com>
2010-12-02 11:50:41 -0800

    refine client transformer combinators

--- src/main/scala/blueeyes/core/service/HttpResponseHandlerCombinators.scala
+++ src/main/scala/blueeyes/core/service/HttpResponseHandlerCombinators.scala
@@ -15,16 +15,16 @@ trait HttpResponseHandlerCombinators{
   def port[T, S](port: Int)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S]    = path(":" + port.toString)(transformer)
 
   def path[T, S](path: String)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] =
-    apply((request: HttpRequest[T]) => request.copy(uri = path + request.uri))(transformer)
+    copyRequest((request: HttpRequest[T]) => request.copy(uri = path + request.uri))(transformer)
 
   def parameters[T, S](parameters: (Symbol, String)*)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] =
-    apply((request: HttpRequest[T]) => request.copy(parameters = Map[Symbol, String](parameters: _*)))(transformer)
+    copyRequest((request: HttpRequest[T]) => request.copy(parameters = Map[Symbol, String](parameters: _*)))(transformer)
 
   def headers[T, S](headers: (String, String)*)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] =
-    apply((request: HttpRequest[T]) => request.copy(headers = Map[String, String](headers: _*)))(transformer)
+    copyRequest((request: HttpRequest[T]) => request.copy(headers = Map[String, String](headers: _*)))(transformer)
 
   def cookies[T, S](cookies: (String, String)*)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = {
-    apply((request: HttpRequest[T]) => {
+    copyRequest((request: HttpRequest[T]) => {
       val cookieEncoder = new CookieEncoder(false);
       cookies.foreach(cookie => cookieEncoder.addCookie(cookie._1, cookie._2))
 
@@ -34,10 +34,10 @@ trait HttpResponseHandlerCombinators{
   }
 
   def remoteHost[T, S](remoteHost: InetAddress)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] =
-    apply((request: HttpRequest[T]) => request.copy(headers = request.headers + Tuple2("X-Forwarded-For", remoteHost.getHostAddress())))(transformer)
+    copyRequest((request: HttpRequest[T]) => request.copy(headers = request.headers + Tuple2("X-Forwarded-For", remoteHost.getHostAddress())))(transformer)
 
   def version[T, S](version: HttpVersion)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] =
-    apply((request: HttpRequest[T]) => request.copy(version = version))(transformer)
+    copyRequest((request: HttpRequest[T]) => request.copy(version = version))(transformer)
 
   def contentType[T, S, R](mimeType: MimeType)(transformer: HttpClientTransformer[T, R])(implicit b: Bijection[T, S]): HttpClientTransformer[S, R] = {
     (client: HttpClient[S]) => {
@@ -52,24 +52,24 @@ trait HttpResponseHandlerCombinators{
     }
   }
 
-  def get[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.GET, transformer)
+  def get[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.GET)(transformer)
 
-  def put[T, S](content: T)(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.PUT, transformer, Some(content))
+  def put[T, S](content: T)(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.PUT)(transformer, Some(content))
 
-  def post[T, S](content: T)(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.POST, transformer, Some(content))
+  def post[T, S](content: T)(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.POST)(transformer, Some(content))
 
-  def delete[T, S]()(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.DELETE, transformer)
+  def delete[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.DELETE)(transformer)
 
-  def options[T, S]()(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.OPTIONS, transformer)
+  def options[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.OPTIONS)(transformer)
 
-  def head[T, S]()(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.HEAD, transformer)
+  def head[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.HEAD)(transformer)
 
-  def connect[T, S]()(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.CONNECT, transformer)
+  def connect[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.CONNECT)(transformer)
 
-  def trace[T, S]()(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = method[T, S](HttpMethods.TRACE, transformer)
+  def trace[T, S](transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] = invokeClient[T, S](HttpMethods.TRACE)(transformer)
 
   def custom[T, S](custom: String)(transformer: HttpResponse[T] => Future[S]): HttpClientTransformer[T, S] =
-    method[T, S](HttpMethods.CUSTOM(custom), transformer)
+    invokeClient[T, S](HttpMethods.CUSTOM(custom))(transformer)
 
   def secure[T, S](transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = {
     (client: HttpClient[T]) => {
@@ -81,13 +81,23 @@ trait HttpResponseHandlerCombinators{
     }
   }
   
-  private def method[T, S](method: HttpMethod, transformer: HttpResponse[T] => Future[S], content: Option[T] = None) = {
+  def method[T, S](method: HttpMethod)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = copyRequest { (request: HttpRequest[T]) =>
+    request.copy(method = method)
+  } (transformer)
+  
+  def content[T, S](content1: T)(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = {
+    def doCopy(request: HttpRequest[T]): HttpRequest[T] = request.copy(content = Some(content1))
+    
+    copyRequest(doCopy)(transformer)
+  }
+  
+  private def invokeClient[T, S](method: HttpMethod)(transformer: HttpResponse[T] => Future[S], content: Option[T] = None) = {
     (client: HttpClient[T]) => {
       client.apply(HttpRequest[T](method = method, content = content, uri = "")).flatMap(transformer)
     }
   }
 
-  private def apply[T, S](copy: (HttpRequest[T]) => HttpRequest[T])(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = {
+  private def copyRequest[T, S](copy: (HttpRequest[T]) => HttpRequest[T])(transformer: HttpClientTransformer[T, S]): HttpClientTransformer[T, S] = {
     (client: HttpClient[T]) => {
       val wrappedClient = new HttpClient[T] {
         def apply(request: HttpRequest[T]): Future[HttpResponse[T]] = client.apply(copy(request))
