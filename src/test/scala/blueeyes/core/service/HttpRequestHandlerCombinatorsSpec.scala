0cf6110d184d25a15041467ba34164f23303eb77
Jeff Simpson <jeff@socialmedia.com>
2010-12-14 11:40:47 -0700

    Added another combinator test

--- src/test/scala/blueeyes/core/service/HttpRequestHandlerCombinatorsSpec.scala
+++ src/test/scala/blueeyes/core/service/HttpRequestHandlerCombinatorsSpec.scala
@@ -4,31 +4,35 @@ import org.specs.Specification
 import org.specs.matcher.Matchers._
 
 import blueeyes.core.http._
+import blueeyes.core.http.MimeType
+import blueeyes.core.http.MimeTypes._
+import blueeyes.core.data.BijectionsString._
+import blueeyes.json.JsonAST._
 import blueeyes.util.Future
 import blueeyes.util.FutureImplicits
 
-class HttpRequestHandlerCombinatorsSpec extends Specification with HttpRequestHandlerCombinators with RestPathPatternImplicits{
+class HttpRequestHandlerCombinatorsSpec extends Specification with HttpRequestHandlerCombinators with RestPathPatternImplicits {
   "composition of paths" should {
     "have the right type" in {
       val handler: HttpRequestHandler[Int] = {
-        path("/foo/bar") { 
+        path("/foo/bar") {
           path("/baz") {
-            get { (request: HttpRequest[Int]) => 
+            get { (request: HttpRequest[Int]) =>
               Future(HttpResponse[Int]())
             }
           }
         }
       }
-      
+
       handler mustBe handler
     }
   }
 
   "default cookies should propagate correctly" in {
     val defaultValue = "defaultValue"
-    val f = path("/foo/bar") { 
+    val f = path("/foo/bar") {
       cookie('someCookie, defaultValue) { cookieVal =>
-        get { (request: HttpRequest[String]) => 
+        get { (request: HttpRequest[String]) =>
           Future(HttpResponse[String](content=Some(cookieVal)))
         }
       }
@@ -38,14 +42,28 @@ class HttpRequestHandlerCombinatorsSpec extends Specification with HttpRequestHa
   }
 
   "request parameters are available using combinator" in {
-    val f = path("/foo/'bar") { 
+    val f = path("/foo/'bar") {
       parameter[String, String]('bar) { bar =>
-        get { (request: HttpRequest[String]) => 
+        get { (request: HttpRequest[String]) =>
           Future(HttpResponse[String](content=Some(bar)))
         }
       }
     }(HttpRequest[String](HttpMethods.GET, "/foo/blahblah"))
     f.value must eventually(beSomething)
     f.value.get.content must beSome("blahblah")
+  }
+
+  "request parameters are available using produce and parameter combinators" in {
+    val f = path("/foo/'bar") {
+      produce(application/json) {
+        parameter[String, JValue]('bar) { bar =>
+          get { (request: HttpRequest[String]) =>
+            Future(HttpResponse[JValue](content=Some(JString(bar))))
+          }
+        }
+      }
+    }(HttpRequest[String](HttpMethods.GET, "/foo/blahblah"))
+    f.value must eventually(beSomething)
+    f.value.get.content.map(JString(_)) must beSome(JString(""""blahblah""""))
   }
 }
