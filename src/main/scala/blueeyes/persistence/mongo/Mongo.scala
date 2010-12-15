8ac8d01f6956a06ee5b04e5fefc3eb7e75b83ab5
Michael Lagutko <misha@socialmedia.com>
2010-12-15 20:38:22 +0200

    code refactor

--- src/main/scala/blueeyes/persistence/mongo/Mongo.scala
+++ src/main/scala/blueeyes/persistence/mongo/Mongo.scala
@@ -4,10 +4,34 @@ import blueeyes.json.JPath
 import java.lang.String
 import blueeyes.json.JsonAST._
 
+/** The Mongo creates a MongoDatabase by  database name.
+ * <p>
+ * <pre>
+ * lazy val injector = Guice.createInjector(new FilesystemConfiggyModule(ConfiggyModule.FileLoc), new RealMongoModule)
+ *
+ * val mongo = injector.getInstance(classOf[Mongo])
+ * </pre>
+ */
 trait Mongo{
   def database(databaseName: String): MongoDatabase
 }
 
+/** The MongoDatabase executes MongoQuery.
+ * <p>
+ * <pre>
+ * import blueeyes.persistence.mongo._
+ * import blueeyes.persistence.mongo.MongoImplicits._
+ * import blueeyes.persistence.mongo.MongoQueryBuilder._
+ *
+ * lazy val injector = Guice.createInjector(new FilesystemConfiggyModule(ConfiggyModule.FileLoc), new RealMongoModule)
+ *
+ * val mongo = injector.getInstance(classOf[Mongo])
+ *
+ * val database  = mongo.database( "mydb" );
+ *
+ * database(selectOne().from("mycollection").sortBy("foo.bar" <<))
+ * </pre
+ */
 trait MongoDatabase{
   def apply[T](query: MongoQuery[T]): T  = {
     val databaseCollection = query.collection match{
@@ -17,10 +41,10 @@ trait MongoDatabase{
     query(databaseCollection)
   }
 
-  def collection(collectionName: String): DatabaseCollection
+  protected def collection(collectionName: String): DatabaseCollection
 }
 
-trait DatabaseCollection{
+private[mongo] trait DatabaseCollection{
   def insert(objects: List[JObject])
   def select(selection : MongoSelection, filter: Option[MongoFilter], sort: Option[MongoSort], skip: Option[Int], limit: Option[Int]): Stream[JObject]
   def group(selection: MongoSelection, filter: Option[MongoFilter], initial: JObject, reduce: String): JArray
@@ -30,7 +54,6 @@ trait DatabaseCollection{
   def ensureIndex(name: String, keys: List[JPath], unique: Boolean)
   def dropIndexes
   def dropIndex(name: String)
-  // seems like there should be upsert here
   def update(filter: Option[MongoFilter], value : MongoUpdate, upsert: Boolean, multi: Boolean)
   def mapReduce(map: String, reduce: String, outputCollection: Option[String], filter: Option[MongoFilter] = None): MapReduceOutput
 }
