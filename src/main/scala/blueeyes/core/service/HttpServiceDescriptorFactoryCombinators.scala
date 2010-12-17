a9319b7aed6a2b8153c572f4c1e1c84ea9693183
Michael Lagutko <misha@socialmedia.com>
2010-12-17 15:40:26 +0200

    HealthMonitor implementation

---
+++ src/main/scala/blueeyes/core/service/HttpServiceDescriptorFactoryCombinators.scala
@@ -0,0 +1,23 @@
+package blueeyes.core.service
+
+import blueeyes.health.HealthMonitor
+import blueeyes.json.JPath
+import net.lag.logging.Logger
+
+trait HttpServiceDescriptorFactoryCombinators {
+  def healthMonitor[T, S](f: HealthMonitor => HttpServiceDescriptorFactory[T, S]): HttpServiceDescriptorFactory[T, S] = {
+    (context: HttpServiceContext) => {
+      val monitor = new HealthMonitor(JPath("%s.v%d.health".format(context.serviceName, context.serviceVersion)))
+
+      f(monitor)(context)
+    }
+  }
+
+  def logging[T, S](f: Logger => HttpServiceDescriptorFactory[T, S]): HttpServiceDescriptorFactory[T, S] = {
+    (context: HttpServiceContext) => {
+      val logger = Logger.configure(context.config.configMap("log"), false, false)
+
+      f(logger)(context)
+    }
+  }
+}
