59ce2acfa8c5cbf25ba976286a8a14bec3dee380
John A. De Goes <john@socialmedia.com>
2010-12-17 10:45:13 -0700

    change plus to join operator

--- src/main/scala/blueeyes/core/service/HttpServiceDescriptor.scala
+++ src/main/scala/blueeyes/core/service/HttpServiceDescriptor.scala
@@ -3,7 +3,7 @@ package blueeyes.core.service
 import blueeyes.util.Future
 
 case class HttpServiceDescriptor[T, S](startup: () => Future[S], request: S => HttpRequestHandler[T], shutdown: S => Future[Unit]) { self =>
-  def + [R](that: HttpServiceDescriptor[T, R]): HttpServiceDescriptor[T, (S, R)] = {
+  def ~ [R](that: HttpServiceDescriptor[T, R]): HttpServiceDescriptor[T, (S, R)] = {
     HttpServiceDescriptor[T, (S, R)](
       startup  = () => self.startup().zip(that.startup()),
       request  = (pair: (S, R)) => self.request(pair._1).orElse(that.request(pair._2)),
@@ -11,7 +11,7 @@ case class HttpServiceDescriptor[T, S](startup: () => Future[S], request: S => H
     )
   }
   
-  def + (that: HttpRequestHandler[T]): HttpServiceDescriptor[T, S] = {
+  def ~ (that: HttpRequestHandler[T]): HttpServiceDescriptor[T, S] = {
     HttpServiceDescriptor[T, S](
       startup  = self.startup,
       request  = (s: S) => {
