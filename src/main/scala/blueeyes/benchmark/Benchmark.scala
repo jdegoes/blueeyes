e43494b374710d483cecd6430f3f824ee49b2937
Michael Lagutko <misha@socialmedia.com>
2010-12-28 17:28:09 +0200

    Benchmark implementation

--- src/main/scala/blueeyes/benchmark/Benchmark.scala
+++ src/main/scala/blueeyes/benchmark/Benchmark.scala
@@ -12,7 +12,7 @@ import blueeyes.json.JsonParser.{parse => j}
 object Benchmark extends ServerStart{
 
   private val threadsCount = 4
-  private val contactsSize = 200
+  private val contactsSize = 600
 
   def main(args: Array[String]){
     startServer
@@ -51,6 +51,7 @@ class BenchmarkTask(val port: Int, val contactsStream: Stream[Contact], val time
     timer.time{
       process({c: Contact => create(c)})
       process({c: Contact => contact(c.name)})
+      process({c: Contact => search(j("""{ "name" : "%s" }""".format(c.name)))})
       process({c: Contact => remove(c.name)})
     }
 
