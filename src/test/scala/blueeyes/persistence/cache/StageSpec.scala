2570b1e39189b9030fcc157f88cfba5a997a5958
Michael Lagutko <misha@socialmedia.com>
2010-12-29 06:19:36 +0200

    cache test suite is added

--- src/test/scala/blueeyes/persistence/cache/StageSpec.scala
+++ src/test/scala/blueeyes/persistence/cache/StageSpec.scala
@@ -6,78 +6,83 @@ import java.util.concurrent.CountDownLatch
 
 class StageSpec extends Specification{
 
-  "Stage.+=: coalesces entries" in{
-    val stage = newStage()
+  private var stage: Stage[String, String] = _
 
-    stage += (("foo", "baz"))
+  "Stage" should {
+    doAfter{
+      stage.stop
+    }
+    "+=: coalesces entries" in{
+      stage = newStage()
 
-    stage.get("foo") must beSome("baz-bar")
-  }
-  "Stage.+=: adds entry" in{
-    val stage = newStage()
+      stage += (("foo", "baz"))
 
-    stage.get("foo") must beSome("bar")
-  }
-  "Stage.-=: removes entry" in{
-    val stage = newStage()
+      stage.get("foo") must beSome("baz-bar")
+    }
+    "+=: adds entry" in{
+      stage = newStage()
 
-    stage -= ("foo")
+      stage.get("foo") must beSome("bar")
+    }
+    "-=: removes entry" in{
+      stage = newStage()
 
-    stage.get("foo") must beNone
-  }
-  "Stage.iterator: returns all entries" in{
-    val stage = newStage()
+      stage -= ("foo")
 
-    stage.iterator.next must beEqual(("foo", "bar"))
-  }
-  "Stage.stop: evicts entries" in{
-    var evicted = false
+      stage.get("foo") must beNone
+    }
+    "iterator: returns all entries" in{
+      stage = newStage()
 
-    val stage = newStage(None, None, {(key: String, value: String) => evicted = key == "foo" && value == "bar"})
+      stage.iterator.next must beEqual(("foo", "bar"))
+    }
+    "stop: evicts entries" in{
+      var evicted = false
 
-    stage.stop
+      stage = newStage(None, None, {(key: String, value: String) => evicted = key == "foo" && value == "bar"})
 
-    evicted must be (true)
-  }
-  "Stage.getLater: gets value later" in{
-    val stage = newStage()
+      stage.stop
 
-    val future = stage.getLater("foo")
+      evicted must be (true)
+    }
+    "getLater: gets value later" in{
+      stage = newStage()
 
-    val latch = new CountDownLatch(1)
-    future.deliverTo({f => latch.countDown()})
+      val future = stage.getLater("foo")
 
-    future.value  must beSome(Some("bar"))
-  }
-  "Stage: evicts when idle time is expired" in{
-    val stage = newStage(Some(1000))
+      val latch = new CountDownLatch(1)
+      future.deliverTo({f => latch.countDown()})
 
-    Thread.sleep(2000)
+      future.value must beSome(Some("bar"))
+    }
+    "evicts when idle time is expired" in{
+      stage = newStage(Some(1000))
 
-    stage.get("foo")  must beNone
-  }
-  "Stage: evicts when live time is expired" in{
-    val stage = newStage(None, Some(1000))
+      Thread.sleep(2000)
 
-    Thread.sleep(2000)
+      stage.get("foo")  must beNone
+    }
+    "evicts when live time is expired" in{
+      stage = newStage(None, Some(1000))
 
-    stage.get("foo")  must beNone
-  }
-  "Stage: evict is called when entry is expired" in{
-    var evicted = false
-    val stage = newStage(None, Some(1000), {(key: String, value: String) => evicted = key == "foo" && value == "bar"})
+      Thread.sleep(2000)
 
-    Thread.sleep(2000)
+      stage.get("foo")  must beNone
+    }
+    "evict is called when entry is expired" in{
+      var evicted = false
+      stage = newStage(None, Some(1000), {(key: String, value: String) => evicted = key == "foo" && value == "bar"})
 
-    stage.get("foo")
+      Thread.sleep(2000)
 
-    evicted must be (true)
+      stage.get("foo")
+
+      evicted must be (true)
+    }
   }
 
   private def newStage(timeToIdle: Option[Long] = None, timeToLive: Option[Long] = None, evict: (String, String) => Unit = {(key: String, value: String) => }) = {
     val stage = new Stage[String, String](settings(timeToIdle, timeToLive, evict), (key: String, v1: String, v2: String) => v1 + "-" + v2)
-
-    stage.start
 
     stage += (("foo", "bar"))
 
