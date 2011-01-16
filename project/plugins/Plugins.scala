8deabc8908153186b0d241ded4497cace4398e56
John A. De Goes <john@socialmedia.com>
2011-01-16 09:38:22 -0700

    update sbt idea plugin

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,8 +1,7 @@
 import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
-  val eclipsify = "de.element34" % "sbt-eclipsify" % "0.6.0"
-  val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
-  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
+  val eclipsify = "de.element34"          % "sbt-eclipsify"   % "0.6.0"
+  val sbtIdea   = "com.github.mpeltonen"  % "sbt-idea-plugin" % "0.2.0"
 }
 
