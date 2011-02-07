327b71f5817fcbd4e62f4a5963e53b2fe11beb8e
Jeff Simpson <jeff@socialmedia.com>
2011-02-07 10:57:26 -0700

    Removed test for upsert with dotted field name

--- src/test/scala/blueeyes/persistence/mongo/mock/MockMongoSpec.scala
+++ src/test/scala/blueeyes/persistence/mongo/mock/MockMongoSpec.scala
@@ -16,15 +16,4 @@ class MockMongoSpec extends Specification{
 
     mongo.database("foo") must be (mongo.database("foo"))
   }
-
-  "Upsert group and select should return the same object" in {
-    val mongo = new MockMongo()
-    val database = mongo.database("testUpsert")
-    val collection = "test"
-
-    val testObj = JObject(JField("properties.groupId", JString("foo")) :: Nil)
-    database[JNothing.type](upsert(collection).set(testObj).where("groupId" === "foo"))
-    val result = database(selectOne().from(collection))
-    result.get must beEqual(testObj)
-  }
 }
