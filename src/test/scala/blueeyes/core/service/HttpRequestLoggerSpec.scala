5e5385c08d2c4a78b4daeccd84f3755998f71f24
Michael Lagutko <misha@socialmedia.com>
2011-03-04 18:22:24 +0200

    HttpRequestLogger implementation

--- src/test/scala/blueeyes/core/service/HttpRequestLoggerSpec.scala
+++ src/test/scala/blueeyes/core/service/HttpRequestLoggerSpec.scala
@@ -1,7 +1,28 @@
 package blueeyes.core.service
 
 import org.specs.Specification
+import blueeyes.parsers.W3ExtendedLogAST._
+import blueeyes.core.http._
+import blueeyes.util.{Future, ClockMock}
+import org.joda.time.format.DateTimeFormat
 
-class HttpRequestLoggerSpec extends Specification{
+class HttpRequestLoggerSpec extends Specification with ClockMock{
 
+  private val DateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
+  private val TimeFormatter = DateTimeFormat.forPattern("HH:mm:ss.S")
+
+  private val request  = HttpRequest[String](method = HttpMethods.GET, uri = "/foo/bar?param=value")
+  private val response = Future.lift(HttpResponse[String]())
+
+  "HttpRequestLogger: logs multiple values" in {
+    log(DateIdentifier, TimeIdentifier).value must eventually (beSome(DateFormatter.print(clockMock.now()) + " " + TimeFormatter.print(clockMock.now())))
+  }
+  "HttpRequestLogger: logs date" in {
+    log(DateIdentifier).value must eventually (beSome(DateFormatter.print(clockMock.now())))
+  }
+  "HttpRequestLogger: logs time" in {
+    log(TimeIdentifier).value must eventually (beSome(TimeFormatter.print(clockMock.now())))
+  }
+
+  private def log(fieldIdentifiers: FieldIdentifier*): Future[String] = HttpRequestLogger[String, String](FieldsDirective(List(fieldIdentifiers: _*)))(clockMock)(request, response)
 }
