9a4cf32737f2d82b5d5301d34f1c5a5a0b89d70f
Jeff Simpson <jeff@socialmedia.com>
2011-03-22 08:28:23 -0600

    Force use of name passed in args

--- src/main/scala/blueeyes/util/logging/LoggingHelper.scala
+++ src/main/scala/blueeyes/util/logging/LoggingHelper.scala
@@ -4,17 +4,19 @@ import net.lag.configgy.{ Config, ConfigMap }
 import net.lag.logging.Logger
 
 object LoggingHelper {
-  def initializeLogging(config: ConfigMap, defaultName: String) = {
+  def initializeLogging(config: ConfigMap, loggerName: String) = {
     val conf = if(config.contains("log")) {
       config.configMap("log")
     } else {
       val localConf = new Config()
-      localConf.setString("node", defaultName)
       localConf.setString("level", "info")
       localConf.setBool("console", true)
       localConf.setBool("use_parents", false)
       localConf
     }
+
+    // Always use passed name
+    conf.setString("node", loggerName)
 
     Logger.configure(conf, false, true)
   }
