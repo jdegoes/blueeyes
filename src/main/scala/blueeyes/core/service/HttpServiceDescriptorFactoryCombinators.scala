a9931afb3ca32890e753d3d134ccbeadb00f2173
Michael Lagutko <misha@socialmedia.com>
2011-04-07 01:27:26 +0300

    services health uptimeSeconds is added

--- src/main/scala/blueeyes/core/service/HttpServiceDescriptorFactoryCombinators.scala
+++ src/main/scala/blueeyes/core/service/HttpServiceDescriptorFactoryCombinators.scala
@@ -36,15 +36,17 @@ trait HttpServiceDescriptorFactoryCombinators extends HttpRequestHandlerCombinat
 
       val underlying = f(monitor)(context)
       val descriptor = underlying.copy(request = (state: S) => {new MonitorHttpRequestHandler(underlying.request(state), monitor)})
+      val startTime = System.currentTimeMillis
 
       descriptor ~> path("/blueeyes/services/" + context.serviceName + "/v" + context.serviceVersion.majorVersion + "/health") {
         get {
           request: HttpRequest[T] => {
-            val version = context.serviceVersion
-            val who     = JObject(JField("service", JObject(JField("name", JString(context.serviceName)) :: JField("version", JString("%d.%d.%s".format(version.majorVersion, version.minorVersion, version.version))) :: Nil)) :: Nil)
-            val server  = JObject(JField("server", JObject(JField("hostName", JString(context.hostName)) :: JField("port", context.port) :: JField("sslPort", context.sslPort) :: Nil)) :: Nil)
-            val health  = monitor.toJValue
-            HttpResponse[T](content=Some(jValueBijection(health.merge(who).merge(server))))
+            val version       = context.serviceVersion
+            val who           = JObject(JField("service", JObject(JField("name", JString(context.serviceName)) :: JField("version", JString("%d.%d.%s".format(version.majorVersion, version.minorVersion, version.version))) :: Nil)) :: Nil)
+            val server        = JObject(JField("server", JObject(JField("hostName", JString(context.hostName)) :: JField("port", context.port) :: JField("sslPort", context.sslPort) :: Nil)) :: Nil)
+            val uptimeSeconds = JObject(JField("uptimeSeconds", JInt((System.currentTimeMillis - startTime) / 1000)) :: Nil)
+            val health        = monitor.toJValue
+            HttpResponse[T](content=Some(jValueBijection(health.merge(who).merge(server).merge(uptimeSeconds))))
           }
         }
       }
