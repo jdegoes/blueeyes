fdc9350e4c89de5a486c82a11747260ebe1fc1a9
Kris Nuttycombe <kris@hylotech.com>
2011-05-18 20:15:56 -0600

    Simplified Done/Retry logic for future matchers.

--- src/main/scala/blueeyes/concurrent/test/FutureMatchers.scala
+++ src/main/scala/blueeyes/concurrent/test/FutureMatchers.scala
@@ -17,41 +17,57 @@ trait FutureMatchers {
 
   implicit val defaultFutureTimeouts: FutureTimeouts = FutureTimeouts(10, 100L.milliseconds)
 
+  private sealed trait Outcome
+  private case class Done(matchResult: (Boolean, String, String)) extends Outcome
+  private case class Retry(failureMessage: String) extends Outcome
+
   /*
    analytics.get("/foo") must deliver {
      beSome(2)
    }
    */
-  case class whenDelivered[A](inner: Matcher[Option[A]])(implicit timeouts: FutureTimeouts) extends Matcher[Future[A]]() {
+  case class whenDelivered[A](inner: Matcher[A])(implicit timeouts: FutureTimeouts) extends Matcher[Future[A]]() {
     def apply(future: => Future[A]): (Boolean, String, String) = {
+      println(timeouts)
       retry(future, timeouts.retries)
     }
 
     @tailrec
     private def retry(future: => Future[A], retries: Int): (Boolean, String, String) = {
+      println("retries: " + retries)
       val delivered = future
 
       val latch = new CountDownLatch(1)
 
       delivered.deliverTo(_ => latch.countDown())
+      delivered.ifCanceled(_ => latch.countDown())
 
       val result = try {
         latch.await(timeouts.timeout.time.toLong, timeouts.timeout.unit)
+        
+        delivered.value match {
+          case Some(value) => 
+            val result = inner(value) 
 
-        inner(delivered.value) |> { result =>
-          if (result.success || retries <= 0) success(result)
-          else failure(result.koMessage)
+            if (result.success || retries <= 0) Done(result)
+            else Retry(result.koMessage)
+
+          case None => 
+            Retry("Delivery of future was canceled on retry " + (timeouts.retries - retries) + ": " + delivered.error)
         }
       } catch {        
-        case (_ : TimeoutException | _ : InterruptedException) => failure("Delivery of future timed out")
+        case (_ : TimeoutException | _ : InterruptedException) => Retry("Delivery of future timed out")
+        case ex: Throwable => 
+          ex.printStackTrace
+          Retry("Got something weird: " + ex.getMessage)
       }  
 
       result match {
-        case Success(result) => result
+        case Done(result) => result
 
-        case Failure(_) if (retries > 0) => retry(future, retries - 1)
+        case Retry(_) if (retries > 0) => retry(future, retries - 1)
 
-        case Failure(message) => (false, "Enough timeout exceptions.", message)
+        case Retry(message) => (false, "Enough timeout exceptions.", message)
       }
     }
   }
