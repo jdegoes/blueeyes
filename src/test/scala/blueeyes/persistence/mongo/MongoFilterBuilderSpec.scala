cf3818fb39a9f2816f98045e316a8b33fbb6db0d
John A. De Goes <john@degoes.net>
2011-05-23 20:35:58 -0600

    test cleanup

--- src/test/scala/blueeyes/persistence/mongo/MongoFilterBuilderSpec.scala
+++ src/test/scala/blueeyes/persistence/mongo/MongoFilterBuilderSpec.scala
@@ -9,31 +9,31 @@ import MongoFilterImplicits._
 class MongoFilterBuilderSpec extends Specification{
 
   "builds $eq operation" in {
-    MongoFilterBuilder(JPath("foo"))===("bar") mustEqual (MongoFieldFilter("foo", $eq, MongoPrimitiveString("bar")))
+    JPath("foo") === "bar" mustEqual (MongoFieldFilter("foo", $eq, MongoPrimitiveString("bar")))
   }
   "builds $ne operation" in {
-    MongoFilterBuilder(JPath("foo")).!==(1) mustEqual (MongoFieldFilter("foo", $ne, MongoPrimitiveInt(1)))
+    JPath("foo") != 1 mustEqual (MongoFieldFilter("foo", $ne, MongoPrimitiveInt(1)))
   }
   "builds $gt operation" in {
-    MongoFilterBuilder(JPath("foo")).>(1l) mustEqual (MongoFieldFilter("foo", $gt, MongoPrimitiveLong(1l)))
+    JPath("foo") > 1l mustEqual (MongoFieldFilter("foo", $gt, MongoPrimitiveLong(1l)))
   }
   "builds $gte operation" in {
-    MongoFilterBuilder(JPath("foo")).>=(1.1) mustEqual (MongoFieldFilter("foo", $gte, MongoPrimitiveDouble(1.1)))
+    JPath("foo") >= 1.1 mustEqual (MongoFieldFilter("foo", $gte, MongoPrimitiveDouble(1.1)))
   }
   "builds $in operation" in {
-    MongoFilterBuilder(JPath("foo")).anyOf(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $in, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
+    JPath("foo").anyOf(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $in, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
   }
   "builds $all operation" in {
-    MongoFilterBuilder(JPath("foo")).contains(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $all, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
+    JPath("foo").contains(MongoPrimitiveString("foo"), MongoPrimitiveString("bar")) mustEqual (MongoFieldFilter("foo", $all, MongoPrimitiveArray(MongoPrimitiveString("foo") :: MongoPrimitiveString("bar") :: Nil)))
   }
 
   "builds $size operation" in {
-    MongoFilterBuilder(JPath("foo")).hasSize(1) mustEqual (MongoFieldFilter("foo", $size, MongoPrimitiveInt(1)))
+    JPath("foo").hasSize(1) mustEqual (MongoFieldFilter("foo", $size, MongoPrimitiveInt(1)))
   }
   "builds $exists operation" in {
     MongoFilterBuilder(JPath("foo")).exists mustEqual (MongoFieldFilter("foo", $exists, MongoPrimitiveBoolean(true)))
   }
   "builds $hasType operation" in {
-    MongoFilterBuilder(JPath("foo")).hasType[JString] mustEqual (MongoFieldFilter("foo", $type, MongoPrimitiveInt(2)))
+    JPath("foo").hasType[JString] mustEqual (MongoFieldFilter("foo", $type, MongoPrimitiveInt(2)))
   }
 }
