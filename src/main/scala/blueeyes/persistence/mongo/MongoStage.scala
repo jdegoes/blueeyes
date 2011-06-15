e3acf4b6bd3b5ff830c871accd11786fdd07f7fc
Kris Nuttycombe <kris@hylotech.com>
2011-06-15 09:17:10 -0600

    Made database and collection public members of MongoStage.

--- src/main/scala/blueeyes/persistence/mongo/MongoStage.scala
+++ src/main/scala/blueeyes/persistence/mongo/MongoStage.scala
@@ -7,7 +7,7 @@ case class MongoStageSettings(expirationPolicy: ExpirationPolicy, maximumCapacit
 
 /** A stage for updates to Mongo.
  */
-class MongoStage (database: MongoDatabase, collection: MongoCollection, mongoStageSettings: MongoStageSettings)
+class MongoStage (val database: MongoDatabase, val collection: MongoCollection, mongoStageSettings: MongoStageSettings)
 extends Stage[MongoFilter, MongoUpdate] {
 
   def flush(filter: MongoFilter, update: MongoUpdate) = {
