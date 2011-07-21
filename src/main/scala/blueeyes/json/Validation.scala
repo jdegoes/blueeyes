67bdca024086c19b7e3171f78fb402b682f86662
Kris Nuttycombe <kris@hylotech.com>
2011-07-21 11:58:34 -0600

    Before type-member JManifests.

--- src/main/scala/blueeyes/json/Validation.scala
+++ src/main/scala/blueeyes/json/Validation.scala
@@ -3,7 +3,7 @@ package blueeyes.json {
 /**
  * This object provides an easy way to validate the extraction of data from JSON.
  * <p>
- * val street = (!json \ "address" \ "street" --> classOf[JString]).values
+ * val street = (!json \ "address" \ "street" asUnsafe JString).values
  * <p>
  * If any field is missing or does not have the appropriate type, a very 
  * descriptive ValidationError will be thrown that contains the exact reason 
@@ -42,8 +42,8 @@ object Validation {
      * Returns the element as a JValue of the specified class, throwing a 
      * ValidationError as necessary.
      */
-    def --> [A <: JValue](clazz: Class[A]): A = {
-      if (value.getClass == clazz) value.asInstanceOf[A] else failType(path, clazz, value.getClass)
+    def asUnsafe[A <: JValue](implicit manifest: JManifest[A]): A = manifest(value).getOrElse {
+      throw ValidationError("Expected ${path} to have type " + manifest + " but was: " + value, path)
     }
     
     /**
@@ -58,10 +58,9 @@ object Validation {
      * because it generates the appropriate path structure necessary for 
      * informative errors.
      */
-    def !! (i: Int) = JPathValue(path + "[" + i + "]", (this --> classOf[JArray]).elements(i))
+    def !! (i: Int) = JPathValue(path + "[" + i + "]", (this asUnsafe JArray).elements(i))
     
     private def failFind[T](path: String): T = throw ValidationError("Expected to find ${path}, but it did not exist", path)
-    private def failType[T](path: String, etype: Class[_], atype: Class[_]): T = throw ValidationError("Expected ${path} to have type " + etype.toString + " but was: " + atype.toString, path)
   }
   
   /**
@@ -70,7 +69,7 @@ object Validation {
    */
   def arrayFieldMap[A](name: String, json: JPathValue, f: JPathValue => A): List[A] = {
     val field = json \ name 
-    val array = field --> classOf[JArray]
+    val array = field asUnsafe JArray
     
     array.elements.foldLeft[(Int, List[A])]((0, Nil)) { (cur, e) =>
       (cur._1 + 1, f(field !! cur._1) :: cur._2)
@@ -81,7 +80,7 @@ object Validation {
    * Utility function to extract the specified field as a JString, then return 
    * the string.
    */
-  def stringField(name: String, json: JPathValue) = (json \ name --> classOf[JString]).value
+  def stringField(name: String, json: JPathValue) = (json \ name asUnsafe JString).value
   
   /**
    * Utility function to extract the specified field as a JInt or JDouble, then
@@ -97,13 +96,13 @@ object Validation {
    * Utility function to extract the specified field as a JInt, then return the
    * integer as a BigInt.
    */
-  def integerField(name: String, json: JPathValue) = (json \ name --> classOf[JInt]).value
+  def integerField(name: String, json: JPathValue) = (json \ name asUnsafe JInt).value
   
   /**
    * Utility function to extract the specified field as a JDouble, then return 
    * the double as a Double.
    */
-  def doubleField(name: String, json: JPathValue) = (json \ name --> classOf[JDouble]).value
+  def doubleField(name: String, json: JPathValue) = (json \ name asUnsafe JDouble).value
   
   /**
    * Utility operator to convert a JValue into a validated JPathValue.
--- src/test/scala/blueeyes/json/ArbitraryJValue.scala
+++ src/test/scala/blueeyes/json/ArbitraryJValue.scala
@@ -43,14 +43,21 @@ trait ArbitraryJValue {
   def genFieldList = Gen.containerOfN[List, JField](listSize, genField)
   def genField = for (name <- alphaStr; value <- genJValue; id <- choose(0, 1000000)) yield JField(name+id, value)
 
-  def genJValueClass: Gen[Class[_ <: JValue]] = oneOf(
-    JNull.getClass.asInstanceOf[Class[JValue]], JNothing.getClass.asInstanceOf[Class[JValue]], classOf[JInt], 
-    classOf[JDouble], classOf[JBool], classOf[JString], classOf[JField], classOf[JArray], classOf[JObject])
+  def genJManifest: Gen[Manifest[_ <: JValue]] = oneOf(
+    JManifest.JNothingM
+    JManifest.JNullM   
+    JManifest.JBoolM   
+    JManifest.JIntM    
+    JManifest.JDoubleM 
+    JManifest.JStringM 
+    JManifest.JArrayM  
+    JManifest.JObjectM 
+  )
 
   def listSize = choose(0, 5).sample.get
 
   implicit def arbJValue: Arbitrary[JValue] = Arbitrary(genJValue)
   implicit def arbJObject: Arbitrary[JObject] = Arbitrary(genObject)
-  implicit def arbJValueClass: Arbitrary[Class[_ <: JValue]] = Arbitrary(genJValueClass)
+  implicit def arbJManifest: Arbitrary[JManifest[_ <: JValue]] = Arbitrary(genJManifest)
   implicit def shrinkJValueClass[T]: Shrink[T] = Shrink(x => Stream.empty)
 }
