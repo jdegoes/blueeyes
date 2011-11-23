486a1507345d994be8093eedc1ba425537262ae2
Kris Nuttycombe <kris@reportgrid.com>
2011-11-23 13:02:10 -0700

    Documented Stoppable.

--- src/main/scala/blueeyes/bkka/Stoppable.scala
+++ src/main/scala/blueeyes/bkka/Stoppable.scala
@@ -4,10 +4,20 @@ import akka.actor.Actor
 import akka.dispatch.Future
 import scala.collection.immutable.Queue
 
+/**
+ * A base trait for typeclass instances that describe how to stop a given type of service.
+ * TODO: make polymorphic in the type of the Future, perhaps using an ADT such as StopResult { StopSuccess / StopFailure }
+ * 
+ * In order for implementations of this trait to work properly in a graph where the same service is a dependant of multiple other services, 
+ * it should be safe for stop to be called multiple times with the same target.
+ */
 trait Stop[-A] {
   def stop(a: A): Future[Any]
 }
 
+/** 
+ * A DAG of things that can be stopped. Dependents are traversed in breadth-first order.
+ */
 sealed trait Stoppable {
   protected def stop: Future[Any]
   def dependents: List[Stoppable]
@@ -19,7 +29,17 @@ object Stoppable {
     def dependents = deps
   }
 
-  def stop(stoppable: Stoppable)(implicit timeout: Actor.Timeout) = {
+  /**
+   * Stop the specified stoppable, returning a future containing a list of the results
+   * of the stoppable graph in breadth-first order. When this future is completed, 
+   * everything will be stopped.
+   *
+   * TODO: Make it possible to specify whether failure to stop any given service
+   * should prevent the stopping of its dependants. At present, any exception encountered
+   * in stopping will stop the stopping process, leaving the system in a potentially 
+   * indeterminate state.
+   */
+  def stop(stoppable: Stoppable)(implicit timeout: Actor.Timeout): Future[List[Any]] = {
     def _stop(q: Queue[List[Stoppable]]): Future[List[Any]] = {
       if (q.isEmpty) Future(Nil)
       else {
