6f09fccbe14c949e24dfa82511986145eb594fc5
Michael Lagutko <misha@socialmedia.com>
2011-11-24 15:37:05 +0300

    WebSocket implementation

--- src/main/scala/blueeyes/core/service/engines/WebSocketNettyServer.scala
+++ src/main/scala/blueeyes/core/service/engines/WebSocketNettyServer.scala
@@ -3,14 +3,12 @@ package blueeyes.core.service.engines
 import org.jboss.netty.channel.group.ChannelGroup
 import blueeyes.core.service.HttpServer
 
-class WebSocketServerProvider(server: HttpServer) extends NettyServerProvider{
-  def startEngine(channelGroup: ChannelGroup) = {
-
-  }
+class WebSocketServerProvider(server: HttpServer) extends AbstractNettyServerProvider{
+  def pipelineFactory(channelGroup: ChannelGroup) = null
 
   def engineType = "ws"
 
-  def enginePort = server.config.getInt("wsPort", 8888)
-
-  def log = server.log
+  def enginePort = server.config.getInt("wsPort", 8889)
 }
+
+class WebSocketNettyServer(server: HttpServer) extends NettyServer(new WebSocketServerProvider(server))
