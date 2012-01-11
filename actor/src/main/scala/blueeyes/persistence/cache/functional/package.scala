1178467b2c43984b2c16ed424eb7fcb25ed6fc8a
Kris Nuttycombe <kris@reportgrid.com>
2012-01-10 23:00:19 -0700

    Split BlueEyes into subprojects to allow for easier isolation of dependencies.

---
+++ actor/src/main/scala/blueeyes/persistence/cache/functional/package.scala
@@ -0,0 +1,8 @@
+package blueeyes.persistence.cache
+
+import blueeyes.actor._
+
+package object functional {
+  type StageActor[K, V] = Actor     [StageIn[K, V], Map[K, V]]
+  type StageState[K, V] = ActorState[StageIn[K, V], Map[K, V]]
+}
---
+++ project/Build.scala
@@ -0,0 +1,38 @@
+import sbt._
+import Keys._
+import sbtassembly.Plugin.AssemblyKeys._
+import sbt.NameFilter._
+
+object BlueEyesBuild extends Build {
+  val scalaz = com.samskivert.condep.Depends(
+    ("scalaz", "core", "org.scalaz"                  %% "scalaz-core"        % "7.0-SNAPSHOT")
+  )
+
+  val nexusSettings : Seq[Project.Setting[_]] = Seq(
+    resolvers ++= Seq("ReportGrid repo"          at   "http://nexus.reportgrid.com/content/repositories/releases",
+                      "ReportGrid repo (public)" at   "http://nexus.reportgrid.com/content/repositories/public-releases",
+                      "ReportGrid snapshot repo"          at   "http://nexus.reportgrid.com/content/repositories/snapshots",
+                      "ReportGrid snapshot repo (public)" at   "http://nexus.reportgrid.com/content/repositories/public-snapshots"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".rgcredentials"),
+    publishTo <<= (version) { version: String =>
+      val nexus = "http://nexus.reportgrid.com/content/repositories/"
+      if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus+"public-snapshots/") 
+      else                                   Some("releases"  at nexus+"public-releases/")
+    }
+  )
+
+  lazy val blueeyes = Project(id = "blueeyes", base = file(".")) aggregate(core, json, mongo)
+  
+  val jsonSettings = nexusSettings ++ Seq(libraryDependencies ++= scalaz.libDeps)
+  lazy val json  = scalaz.addDeps(Project(id = "json", base = file("json")).settings(jsonSettings : _*))
+
+  lazy val core  = Project(id = "core", base = file("core")).settings(nexusSettings : _*) dependsOn json
+
+  lazy val mongo = Project(id = "mongo", base = file("mongo")).settings(nexusSettings : _*) dependsOn (core, json % "test->test")
+  
+  val actorSettings = nexusSettings ++ Seq(libraryDependencies ++= scalaz.libDeps)
+  lazy val actor = scalaz.addDeps(Project(id = "actor", base = file("actor")).settings(actorSettings : _*))
+}
+
+
+// vim: set ts=4 sw=4 et:
