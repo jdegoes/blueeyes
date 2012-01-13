14281d7e4b257431598d3378c36d4af5bc1431d1
Nick Matheson <nick@reportgrid.com>
2012-01-13 12:03:50 -0700

    Minor code cleanup

--- core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
+++ core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
@@ -6,7 +6,7 @@ import akka.dispatch.MessageDispatcher
 import scalaz._
 
 object AkkaTypeClasses {
-  implicit def futureApplicative[A](implicit context: MessageDispatcher) = new Applicative[Future] {
+  implicit def futureApplicative(implicit context: MessageDispatcher) = new Applicative[Future] {
     def point[A](a: => A) = Future(a)(context) 
     def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
   }
