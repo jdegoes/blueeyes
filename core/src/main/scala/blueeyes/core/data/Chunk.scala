4fbc45a9342260f2184e455f34029591fadc5bd2
Kris Nuttycombe <kris@reportgrid.com>
2012-02-27 16:08:50 -0700

    Added method to extend a chunk, splitting the appended value if appending it would create a chunk that is too large.

--- core/src/main/scala/blueeyes/core/data/Chunk.scala
+++ core/src/main/scala/blueeyes/core/data/Chunk.scala
@@ -5,12 +5,56 @@ import akka.dispatch.ExecutionContext
 import scalaz._
 import scalaz.syntax.semigroup._
 
+trait Incremental[A] extends Semigroup[A] {
+  def length(a: A): Int
+  def isEmpty(a: A): Boolean = length(a) == 0
+  def splitAt(a: A, i: Int): (A, A)
+}
+
 case class Chunk[A](data: A, next: Option[Future[Chunk[A]]] = None) {
   def ++(other: Chunk[A])(implicit ctx: ExecutionContext): Chunk[A] = Chunk(data, next.map(_.map(_ ++ other)).orElse(Some(Future(other))))
 
   def suffix(a: A)(implicit s: Semigroup[A]): Chunk[A] = next match {
     case None       => Chunk(data |+| a)
     case Some(more) => Chunk(data, Some(more map (_ suffix a)))
+  }
+
+  /**
+   * Extends the given chunk by suffixing with the given value. This will attempt to accumulate
+   * as much data in each chunk as possible, up to the specified limit. This will not attempt to split
+   * single elements that exceed the maximum chunk size.
+   */
+  def extend(maxSize: Int)(a: A)(implicit s: Semigroup[A], len: A => Int, ctx: ExecutionContext): Chunk[A] = next match {
+    case None if len(data) + len(a) < maxSize => Chunk(data |+| a)
+    case None                                 => Chunk(data, Some(Future(Chunk(a))))
+    case Some(more)                           => Chunk(data, Some(more map (_.extend(maxSize)(a))))
+  }
+
+  /**
+   * Extend the given chunk by suffixing with the given value. This will attempt to split single elements
+   * that exceed the maximum chunk size; however, it is important to note that if the specified Incremental
+   * is not able to split the specified value (i.e. returns all of the data of an element in a suffix) then
+   * this will result in a runtime error.
+   */
+  def extendSplit(maxSize: Int)(a: A)(implicit incr: Incremental[A], ctx: ExecutionContext): Chunk[A] = {
+    val dlen = incr.length(data)
+    val alen = incr.length(a)
+    next match {
+      case Some(more)                    => Chunk(data, Some(more map (_.extendSplit(maxSize)(a))))
+      case None if dlen + alen < maxSize => Chunk(data |+| a)
+      case None                          =>
+        val (prefix, suffix) = incr.splitAt(a, maxSize - dlen)
+        val sufLen = incr.length(suffix)
+
+        if ((maxSize - dlen) > 0 && sufLen >= alen)         
+          sys.error("Unable to split value into chunks; halting now to prevent unbounded recursion.") 
+        else if (sufLen < maxSize)  
+          Chunk(data |+| prefix, Some(Future(Chunk(suffix))))
+        else {
+          val (sufPref, sufSuf) = incr.splitAt(suffix, maxSize)
+          Chunk(data |+| prefix, Some(Future(Chunk(sufPref).extendSplit(maxSize)(sufSuf))))
+        }
+    }
   }
 
   def map[B](f: A => B): Chunk[B] = Chunk(f(data), next.map(_.map(_.map(f))))
