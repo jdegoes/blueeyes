a29aa08287a20477a8f6b398932cfc313a5688eb
Kris Nuttycombe <kris@reportgrid.com>
2012-07-23 17:02:33 -0600

    Future only requires an ExecutionContext, not a full MessageDispatcher.

--- core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
+++ core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
@@ -1,12 +1,12 @@
 package blueeyes.bkka
 
 import akka.dispatch.Future
-import akka.dispatch.MessageDispatcher
+import akka.dispatch.ExecutionContext
 
 import scalaz._
 
 object AkkaTypeClasses {
-  implicit def futureApplicative(implicit context: MessageDispatcher) = new Applicative[Future] with Monad[Future] {
+  implicit def futureApplicative(implicit context: ExecutionContext) = new Applicative[Future] with Monad[Future] {
     def point[A](a: => A) = Future(a)(context) 
     def bind[A, B](fut: Future[A])(f: A => Future[B]) = fut.flatMap(f)
     override def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
