c45db4f8656345a2213ddf5624318249108a4859
Kris Nuttycombe <kris@reportgrid.com>
2012-07-23 22:12:49 +0000

    Added Monad typeclass for Akka Future.

--- core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
+++ core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
@@ -6,8 +6,9 @@ import akka.dispatch.MessageDispatcher
 import scalaz._
 
 object AkkaTypeClasses {
-  implicit def futureApplicative(implicit context: MessageDispatcher) = new Applicative[Future] {
+  implicit def futureApplicative(implicit context: MessageDispatcher) = new Applicative[Future] with Monad[Future] {
     def point[A](a: => A) = Future(a)(context) 
-    def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
+    def bind[A, B](fut: Future[A])(f: A => Future[B]) = fut.flatMap(f)
+    override def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
   }
 }
