850e7aeefa3dbdba42c84b5407e66af62e2832b7
Kris Nuttycombe <kris@reportgrid.com>
2012-09-05 16:43:25 -0400

    Factored out Monad[Future] so that it can be copointed in the correct circumstances

--- core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
+++ core/src/main/scala/blueeyes/bkka/FutureTypeClasses.scala
@@ -5,10 +5,12 @@ import akka.dispatch.ExecutionContext
 
 import scalaz._
 
+class FutureMonad(implicit context: ExecutionContext) extends Applicative[Future] with Monad[Future] {
+  def point[A](a: => A) = Future(a)(context) 
+  def bind[A, B](fut: Future[A])(f: A => Future[B]) = fut.flatMap(f)
+  override def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
+}
+
 object AkkaTypeClasses {
-  implicit def futureApplicative(implicit context: ExecutionContext) = new Applicative[Future] with Monad[Future] {
-    def point[A](a: => A) = Future(a)(context) 
-    def bind[A, B](fut: Future[A])(f: A => Future[B]) = fut.flatMap(f)
-    override def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = fa.flatMap{ a => ff.map{ f => f(a) } }
-  }
+  implicit def futureApplicative(implicit context: ExecutionContext) = new FutureMonad
 }
