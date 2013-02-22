baf97483acec4aeedf24e7501bd710bed5e4edb8
Kris Nuttycombe <kris@precog.com>
2013-02-21 22:26:00 -0500

    Version bump for M6.10

--- project/Build.scala
+++ project/Build.scala
@@ -67,7 +67,7 @@ object BlueEyesBuild extends Build {
 
     crossScalaVersions := Seq("2.9.2"),
 
-    version := "1.0.0-M6.9",
+    version := "1.0.0-M6.10",
 
     organization := "com.reportgrid",
 
