c50abf6642f206d192dbcfc9882bbea7dff325d6
Kris Nuttycombe <kris@precog.com>
2013-03-18 21:51:06 -0600

    Adds an unsafe Copointed instance for FutureMonad
    This is just for debug & test use.

--- bkka/src/main/scala/blueeyes/bkka/FutureMonad.scala
+++ bkka/src/main/scala/blueeyes/bkka/FutureMonad.scala
@@ -1,7 +1,9 @@
 package blueeyes.bkka
 
 import akka.dispatch.Future
+import akka.dispatch.Promise
 import akka.dispatch.ExecutionContext
+import akka.dispatch.Await
 
 import scalaz._
 
@@ -9,6 +11,12 @@ class FutureMonad(context: ExecutionContext) extends Applicative[Future] with Mo
   def point[A](a: => A) = Future(a)(context)
   def bind[A, B](fut: Future[A])(f: A => Future[B]) = fut.flatMap(f)
   override def ap[A,B](fa: => Future[A])(ff: => Future[A => B]) = (fa zip ff) map { case (a, f) => f(a) }
+}
+
+class UnsafeFutureComonad(context: ExecutionContext, copointMaxWait: akka.util.Duration) extends FutureMonad(context) with Comonad[Future] {
+  def copoint[A](m: Future[A]) = Await.result(m, copointMaxWait)
+  def cojoin[A](a: Future[A]): Future[Future[A]] = Promise.successful(a)(context)
+  def cobind[A, B](fa: Future[A])(f: Future[A] => B): Future[B] = Promise.successful(f(fa))(context)
 }
 
 object FutureMonad {
