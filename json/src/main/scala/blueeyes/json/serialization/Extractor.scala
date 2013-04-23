49e04a60493e2be527aadb00d2bba16f514dd529
Kris Nuttycombe <kris@precog.com>
2013-04-23 16:54:46 -0400

    Added convenience mapv method to Extractor to allow easier composition of extractors.

--- json/src/main/scala/blueeyes/json/serialization/Extractor.scala
+++ json/src/main/scala/blueeyes/json/serialization/Extractor.scala
@@ -31,6 +31,10 @@ trait Extractor[A] { self =>
     def validated(jvalue: JValue) = self.validated(jvalue) map f
   }
 
+  def mapv[B](f: A => Validation[Error, B]): Extractor[B] = new Extractor[B] {
+    def validated(jvalue: JValue) = self.validated(jvalue) flatMap f
+  }
+
   def kleisli = Kleisli[({type λ[+α] = Validation[Error, α]})#λ, JValue, A](validated _)
 
   def apply(jvalue: JValue): A = extract(jvalue)
