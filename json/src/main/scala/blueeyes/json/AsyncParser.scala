50075e39066c6e3121ee9117dd5dd6a24393e1cd
Erik Osheim <d_m@plastic-idolatry.com>
2013-05-09 17:00:50 -0400

    more clean up based on dcsobral's suggestions

--- json/src/main/scala/blueeyes/json/AsyncParser.scala
+++ json/src/main/scala/blueeyes/json/AsyncParser.scala
@@ -45,36 +45,43 @@ object AsyncParser {
       offset = 0, done = false, streamMode = 1)
 }
 
-// We have a lot of fields here.
-// 
-// The (state, curr, stack) triple is used to save and restore parser
-// state between async calls. State also helps encode extra
-// information when streaming or unwrapping an array.
-//
-// The (data, len, allocated) triple is used to manage the underlying
-// data the parser is keeping track of. As new data comes in, data may
-// be expanded if not enough space is available.
-// 
-// The offset parameter is used to drive the outer async parsing. It
-// stores similar information to curr but is kept separate to avoid
-// "corrupting" our snapshot.
-// 
-// The done parameter is used internally to help figure out when the
-// atEof() parser method should return true. This will be set when
-// apply(None) is called.
-// 
-// The streamMode parameter controls how the asynchronous parser will
-// be handling multiple values. There are three states:
-// 
-//    1: An array is being unwrapped. Normal JSON array rules apply
-//       (Note that if the outer value observed is not an array, this
-//       mode will toggle to the -1 mode).
-//
-//    0: A JSON stream is being parsed. JSON values will be separated
-//       by optional whitespace
-//
-//   -1: No streaming is occuring. Only a single JSON value is
-//       allowed.
+/*
+ * AsyncParser is able to parse chunks of data (encoded as
+ * Option[ByteBuffer] instances) and parse asynchronously.  You can
+ * use the factory methods in the companion object to instantiate an
+ * async parser.
+ * 
+ * The async parser's fields are described below:
+ * 
+ * The (state, curr, stack) triple is used to save and restore parser
+ * state between async calls. State also helps encode extra
+ * information when streaming or unwrapping an array.
+ *
+ * The (data, len, allocated) triple is used to manage the underlying
+ * data the parser is keeping track of. As new data comes in, data may
+ * be expanded if not enough space is available.
+ * 
+ * The offset parameter is used to drive the outer async parsing. It
+ * stores similar information to curr but is kept separate to avoid
+ * "corrupting" our snapshot.
+ * 
+ * The done parameter is used internally to help figure out when the
+ * atEof() parser method should return true. This will be set when
+ * apply(None) is called.
+ * 
+ * The streamMode parameter controls how the asynchronous parser will
+ * be handling multiple values. There are three states:
+ * 
+ *    1: An array is being unwrapped. Normal JSON array rules apply
+ *       (Note that if the outer value observed is not an array, this
+ *       mode will toggle to the -1 mode).
+ *
+ *    0: A JSON stream is being parsed. JSON values will be separated
+ *       by optional whitespace
+ *
+ *   -1: No streaming is occuring. Only a single JSON value is
+ *       allowed.
+ */
 final class AsyncParser protected[json] (
   protected[json] var state: Int,
   protected[json] var curr: Int,
